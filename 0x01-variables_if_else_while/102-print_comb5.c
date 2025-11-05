74abc369eee5edf24aa8ef9e849e9e3627ed70c3
David Adesanya <davidjadesanya@gmail.com>
2025-11-05 23:43:24 +0100

    Add 102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					if (i >= k && j >= l)
+					if (i > k || (i == k && j >= l))
 						continue;
 					putchar(i + 48);
 					putchar(j + 48);
