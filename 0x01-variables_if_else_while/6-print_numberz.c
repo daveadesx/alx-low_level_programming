0671d7884957379c08b199500a3ae726136155f0
David Adesanya <davidjadesanya@gmail.com>
2025-11-05 20:18:31 +0100

    Change spaces to tabs

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 48; i <= 57; i++)
-                putchar(i);
-        putchar('\n');
-        return (0);
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
