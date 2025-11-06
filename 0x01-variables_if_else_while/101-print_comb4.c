6bcca0722222eff703251c8ce6c320c2110fe339
David Adesanya <davidjadesanya@gmail.com>
2025-11-06 23:20:27 +0100

    Add 101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i >= j || j >= k || i >= k)
+					continue;
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
