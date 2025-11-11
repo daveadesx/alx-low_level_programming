e5ec77b262133992ba3fba0aea5e996e47e3c52b
David Adesanya <davidjadesanya@gmail.com>
2025-11-11 22:50:27 +0100

    Add 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets x10
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+			_putchar(97 + j);
+		_putchar('\n');
+	}
+}
