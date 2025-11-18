e6079191a4b417a19193511282756e3634dd15a1
David Adesanya <davidjadesanya@gmail.com>
2025-11-18 07:44:59 +0100

    Add 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i, j, ans;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			ans = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				if (ans < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					_putchar(' ');
+			}
+			if (ans >= 10)
+			{
+				_putchar((ans / 10) + '0');
+				_putchar((ans % 10) + '0');
+			}
+			else
+				_putchar(ans + '0');
+		}
+		_putchar('\n');
+	}
+}
+
