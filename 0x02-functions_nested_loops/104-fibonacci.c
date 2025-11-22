482ebbcb9e9fdcf9a51a88f82bb1eefd119a59e3
David Adesanya <davidjadesanya@gmail.com>
2025-11-23 00:22:26 +0100

    HAndle integer overflow byt splitting values in BASE 1000000000

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define BASE 1000000000
+
 /**
  * main - print first 98 fibonacci sequence
  *
@@ -8,20 +10,35 @@
 int main(void)
 {
 	unsigned long prev, current, tmp;
-	int count;
+	int count, carry;
 
-	prev = 1;
-	current = 1;
+	unsigned long p0 = 1, p1 = 0, p2 = 0;
+	unsigned long c0 = 1, c1 = 0, c2 = 0;
+	unsigned long t0 = 0, t1 = 0, t2 = 0;
 
 	for (count = 0; count < 98; count++)
 	{
 		if (count != 0)
 			printf(", ");
-		printf("%lu", current);
+		if (c2 > 0)
+			printf("%lu%09lu%09lu", c2, c1, c0);
+		else if (c1 > 0)
+			printf("%lu%09lu", c1, c0);
+		else
+			printf("%lu", c0);
 
-		tmp =  current;
-		current += prev;
-		prev = tmp;
+		t0 = c0, t1 = c1, t2 = c2;
+		/* calculate the nex value: c = c + p */
+		c0 += p0;
+		carry = c0 / BASE;
+		c0  %= BASE;
+
+		c1 += (carry + p1);
+		carry = c1 /  BASE;
+		c1 %= BASE;
+
+		c2 += (carry + p2);
+		p0 = t0, p1 = t1, p2 = t2;
 
 	}
 	printf("\n");
