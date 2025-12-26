3cbd9d82ef743c58b3decbd8274f20cb4964212b
Adesanya David <davidjadesanya@gmail.com>
2025-12-26 05:14:08 +0100

    Add 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a char is uppercase
+ *
+ * @c: char to be checked
+ * Return: 1 if true, 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 60 & c <= 90)
+		return (1);
+	return (0);
+}
